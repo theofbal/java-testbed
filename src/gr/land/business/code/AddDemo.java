f69d9fd58d142787d336c349e057d907af2a4428
theofbal <theofanhsbalaskas@gmail.com>
2022-10-26 14:29:39 +0300

    AddDemo v 0.1

---
+++ src/gr/land/business/code/AddDemo.java
@@ -0,0 +1,13 @@
+package gr.land.business.code;
+
+public class AddDemo {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int result;
+
+        result = a + b;
+
+        System.out.println(result);
+    }
+}
