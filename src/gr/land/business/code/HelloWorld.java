ffe04173337e33ee9321fde661dff6f88dc5b929
theofbal <theofanhsbalaskas@gmail.com>
2022-10-26 14:19:57 +0300

    Hello World v 0.2

--- src/gr/land/business/code/HelloWorld.java
+++ src/gr/land/business/code/HelloWorld.java
@@ -1,5 +1,9 @@
 package gr.land.business.code;
 
+/**
+ * Prints Hello World
+ * @author Theofanis Balaskas
+ */
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
